e6449f64cc1c2b96eaddc3e2b739d445af09aa48
Liuledian <1650676155@qq.com>
2020-12-03 16:52:45 +0800

    upload

--- audio.py
+++ audio.py
@@ -3,12 +3,20 @@ import wave
 import os
 import requests
 import uuid
+import math
 
+# record params
 audio_dir = os.getenv("HOME") + "/test/audio-data"
 chunk = 1024
 channels = 1
 fs = 16000
 seconds = 3
+# upload params
+slice_size = 4 * 2**20
+productId = "278589295"
+apikey = "cc7d7c3a35654ed6bc7df213c78f9522"
+create_audio_url = "https://lasr.duiopen.com/lasr-file-api/v2/audio"
+upload_audio_url = "https://lasr.duiopen.com/lasr-file-api/v2/audio/{audio_id}/slice/{slice_index}"
 
 
 def record_audio_save(filename):
@@ -46,19 +54,39 @@ def record_audio_save(filename):
     wf.close()
 
 
-def create_audio_on_server():
-    url = "https://lasr.duiopen.com/lasr-file-api/v2/audio"
+def create_audio_on_server(slice_num):
     sid = str(uuid.uuid4())
+    params = {'productId': productId,
+              'apikey': apikey}
     headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-              'x-sessionId': sid}
-    payload = {'audio_type': 'wav', 'slice_num': 1}
-    r = requests.post(url, headers=headers, data=payload)
+               'x-sessionId': sid}
+    payload = {'audio_type': 'wav',
+               'slice_num': slice_num}
+    r = requests.post(create_audio_url, params=params, headers=headers, data=payload)
     print(r.json())
     audio_id = ""
     return audio_id, sid
 
-def upload_audio(filename, sid, slice):
-    url = 'https://lasr.duiopen.com/lasr-file-api/v2/audio/{audio_id}/slice/{slice_index}'.format(audio_id=)
+
+def upload_audio(filename):
+    file_size = os.path.getsize(filename)
+    slice_num = math.ceil(file_size/slice_size)
+    audio_id, sid = create_audio_on_server(slice_num)
+    params = {'productId': productId,
+              'apikey': apikey}
+    headers = {'Content-Type': 'multipart/form-data',
+               'x-sessionId': sid}
+    with open(filename, 'rb') as audio_file:
+        slice_index = 0
+        while file_size > 0:
+            read_size = min(file_size, slice_size)
+            data = audio_file.read(read_size)
+            r = requests.post(upload_audio_url.format(audio_id,slice_index),
+                              params=params, headers=headers, files={'slice': data})
+            print(r.text)
+            file_size -= read_size
+            slice_index += 1
+
 
 if __name__ == '__main__':
     if not os.path.exists(audio_dir):
