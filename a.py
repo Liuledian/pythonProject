97ffa3e2f53a83c585e45bdd9c3f1a00cea7df5d
Liuledian <1650676155@qq.com>
2021-03-09 19:28:34 +0800

    vigenere cipher cracker

--- a.py
+++ a.py
@@ -1,8 +1,43 @@
-def phi(miu):
-    return 3*(miu-4)**4-4*(miu-4)**3-12*(miu-4)**2
+def solution(K, A):
+    N = len(A)
+    M = len(A[0])
+    houses = []
+    for i in range(N):
+        for j in range(M):
+            if A[i][j] == 1:
+                houses.append((i, j))
 
-def phi_dot(miu):
-    return 12*(miu-4)**3 - 12*(miu-4)**2 - 24*(miu-4)
+    result = 0
+    for i in range(N):
+        for j in range(M):
+            flag = True
+            for house in houses:
+                distance = abs(house[0] - i) + abs(house[1] - j)
+                if distance == 0 or distance > K:
+                    flag = False
+                    break
+            if flag:
+                result += 1
+    return result
 
-def bound(alpha, miu):
-    return 832 + alpha * (-864) * miu
+
+a = [[0,0,0,0],
+     [0,0,1,0],
+     [1,0,0,1]]
+
+b= [[0,1],
+    [0,0]]
+
+c= [[0,0,0,1],
+    [0,1,0,0],
+    [0,0,1,0],
+    [1,0,0,0],
+    [0,0,0,0]]
+
+k1 = 2
+k2 = 1
+k3 = 4
+
+print(solution(k1, a))
+print(solution(k2, b))
+print(solution(k3, c))
