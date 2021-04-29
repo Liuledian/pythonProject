cd3691f30944ae2f0176ea672a9a84b0cdb14e9d
Liuledian <1650676155@qq.com>
2021-04-29 18:29:34 +0800

    nn a3 dann

--- DANN/main.py
+++ DANN/main.py
@@ -1,159 +1,194 @@
 import random
 import os
-import sys
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
-import torch.utils.data
+import torch
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
-from data_loader import GetLoader
-from torchvision import datasets
-from torchvision import transforms
+import sklearn.metrics
 from model import CNNModel
-from test import test
-
-source_dataset_name = 'MNIST'
-target_dataset_name = 'mnist_m'
-source_image_root = os.path.join('dataset', source_dataset_name)
-target_image_root = os.path.join('dataset', target_dataset_name)
-model_root = 'models'
-cuda = True
-cudnn.benchmark = True
-lr = 1e-3
-batch_size = 128
-image_size = 28
-n_epoch = 100
-
-manual_seed = random.randint(1, 10000)
-random.seed(manual_seed)
-torch.manual_seed(manual_seed)
-
-# load data
-
-img_transform_source = transforms.Compose([
-    transforms.Resize(image_size),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.1307,), std=(0.3081,))
-])
-
-img_transform_target = transforms.Compose([
-    transforms.Resize(image_size),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-])
-
-dataset_source = datasets.MNIST(
-    root='dataset',
-    train=True,
-    transform=img_transform_source,
-    download=True
-)
-
-dataloader_source = torch.utils.data.DataLoader(
-    dataset=dataset_source,
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers=8)
-
-train_list = os.path.join(target_image_root, 'mnist_m_train_labels.txt')
-
-dataset_target = GetLoader(
-    data_root=os.path.join(target_image_root, 'mnist_m_train'),
-    data_list=train_list,
-    transform=img_transform_target
-)
-
-dataloader_target = torch.utils.data.DataLoader(
-    dataset=dataset_target,
-    batch_size=batch_size,
-    shuffle=True,
-    num_workers=8)
-
-# load model
-
-my_net = CNNModel()
-
-# setup optimizer
-
-optimizer = optim.Adam(my_net.parameters(), lr=lr)
-
-loss_class = torch.nn.NLLLoss()
-loss_domain = torch.nn.NLLLoss()
-
-if cuda:
-    my_net = my_net.cuda()
-    loss_class = loss_class.cuda()
-    loss_domain = loss_domain.cuda()
-
-for p in my_net.parameters():
-    p.requires_grad = True
-
-# training
-best_accu_t = 0.0
-for epoch in range(n_epoch):
-
-    len_dataloader = min(len(dataloader_source), len(dataloader_target))
-    data_source_iter = iter(dataloader_source)
-    data_target_iter = iter(dataloader_target)
-
-    for i in range(len_dataloader):
-
-        p = float(i + epoch * len_dataloader) / n_epoch / len_dataloader
-        alpha = 2. / (1. + np.exp(-10 * p)) - 1
-
-        # training model using source data
-        data_source = data_source_iter.next()
-        s_img, s_label = data_source
-
-        my_net.zero_grad()
-        batch_size = len(s_label)
-
-        domain_label = torch.zeros(batch_size).long()
-
-        if cuda:
-            s_img = s_img.cuda()
-            s_label = s_label.cuda()
-            domain_label = domain_label.cuda()
+import pickle
+import logging
 
 
-        class_output, domain_output = my_net(input_data=s_img, alpha=alpha)
-        err_s_label = loss_class(class_output, s_label)
-        err_s_domain = loss_domain(domain_output, domain_label)
+def get_logger():
+    l = logging.getLogger()
+    l.setLevel(logging.NOTSET)
+    fh = logging.FileHandler(log_path)
+    sh = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    fh.setLevel(logging.NOTSET)
+    sh.setLevel(logging.NOTSET)
+    l.addHandler(fh)
+    l.addHandler(sh)
+    return l
 
-        # training model using target data
-        data_target = data_target_iter.next()
-        t_img, _ = data_target
 
-        batch_size = len(t_img)
+log_path = './log'
+logger = get_logger()
 
-        domain_label = torch.ones(batch_size).long()
 
-        if cuda:
-            t_img = t_img.cuda()
-            domain_label = domain_label.cuda()
+class SeedDataset(Dataset):
+    def __init__(self, data, label):
+        self.data = torch.tensor(data, dtype=torch.float32)
+        # convert label to start from 0
+        self.label = torch.tensor(label + 1, dtype=torch.long)
 
-        _, domain_output = my_net(input_data=t_img, alpha=alpha)
-        err_t_domain = loss_domain(domain_output, domain_label)
-        err = err_t_domain + err_s_domain + err_s_label
-        err.backward()
-        optimizer.step()
+    def __len__(self):
+        return len(self.label)
 
-        sys.stdout.write('\r epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f' \
-              % (epoch, i + 1, len_dataloader, err_s_label.data.cpu().numpy(),
-                 err_s_domain.data.cpu().numpy(), err_t_domain.data.cpu().item()))
-        sys.stdout.flush()
-        torch.save(my_net, '{0}/mnist_mnistm_model_epoch_current.pth'.format(model_root))
+    def __getitem__(self, item):
+        return self.data[item], self.label[item]
 
-    print('\n')
-    accu_s = test(source_dataset_name)
-    print('Accuracy of the %s dataset: %f' % ('mnist', accu_s))
-    accu_t = test(target_dataset_name)
-    print('Accuracy of the %s dataset: %f\n' % ('mnist_m', accu_t))
-    if accu_t > best_accu_t:
-        best_accu_s = accu_s
-        best_accu_t = accu_t
-        torch.save(my_net, '{0}/mnist_mnistm_model_epoch_best.pth'.format(model_root))
 
-print('============ Summary ============= \n')
-print('Accuracy of the %s dataset: %f' % ('mnist', best_accu_s))
-print('Accuracy of the %s dataset: %f' % ('mnist_m', best_accu_t))
-print('Corresponding model was save in ' + model_root + '/mnist_mnistm_model_epoch_best.pth')
+def evaluate_dann(model, dataset, batch_size):
+    dataloader = DataLoader(dataset, batch_size)
+    y_true = []
+    y_pred = []
+    with torch.no_grad():
+        for data, label in dataloader:
+            y_true.extend(label.detach().cpu().numpy())
+            class_output, _ = model(data, 0)
+            y_pred_torch = torch.argmax(class_output, dim=1)
+            y_pred.extend(y_pred_torch.detach().cpu().numpy())
+
+    f1_score = sklearn.metrics.f1_score(y_true, y_pred, average='macro')
+    accuracy = sklearn.metrics.accuracy_score(y_true, y_pred)
+    return accuracy, f1_score
+
+
+def train_dann(dataset_source, dataset_target, n_epoch, batch_size, in_dim, h_dims, out_dim, ckpt_save_path):
+    cuda = False
+    lr = 1e-3
+    l_d = 0.1
+
+    dataloader_source = torch.utils.data.DataLoader(
+        dataset=dataset_source,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    dataloader_target = torch.utils.data.DataLoader(
+        dataset=dataset_target,
+        batch_size=batch_size,
+        shuffle=True,
+   )
+
+    model = CNNModel(in_dim, h_dims, out_dim)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    loss_class = torch.nn.CrossEntropyLoss()
+    loss_domain = torch.nn.CrossEntropyLoss()
+
+    if cuda:
+        model = model.cuda()
+        loss_class = loss_class.cuda()
+        loss_domain = loss_domain.cuda()
+
+    for p in model.parameters():
+        p.requires_grad = True
+
+    # training
+    best_acc = 0.0
+    best_ep = 0
+    tr_acc_ls = []
+    te_acc_ls = []
+    loss_ls = []
+    for epoch in range(n_epoch):
+        model.train()
+        len_dataloader = min(len(dataloader_source), len(dataloader_target))
+        data_source_iter = iter(dataloader_source)
+        data_target_iter = iter(dataloader_target)
+        loss_sum = 0.0
+        n_s = 0
+        for i in range(len_dataloader):
+            # Compute reverse layer parameter alpha
+            p = float(i + epoch * len_dataloader) / n_epoch / len_dataloader
+            alpha = 2. / (1. + np.exp(-10 * p)) - 1
+
+            # training model using source data
+            data_s, label_s = data_source_iter.next()
+            batch_size_s = len(label_s)
+            n_s += batch_size_s
+            domain_label = torch.zeros(batch_size_s).long()
+
+            if cuda:
+                data_s = data_s.cuda()
+                label_s = label_s.cuda()
+                domain_label = domain_label.cuda()
+
+            class_output, domain_output = model(input_data=data_s, alpha=alpha)
+            loss_c = loss_class(class_output, label_s)
+            loss_ds = loss_domain(domain_output, domain_label)
+
+            # training model using target data
+            data_t, _ = data_target_iter.next()
+            batch_size_t = len(data_t)
+            domain_label = torch.ones(batch_size_t).long()
+
+            if cuda:
+                data_t = data_t.cuda()
+                domain_label = domain_label.cuda()
+            _, domain_output = model(input_data=data_t, alpha=alpha)
+            loss_dt = loss_domain(domain_output, domain_label)
+
+            # Compute overall loss and backprop
+            loss = loss_c + l_d * (loss_dt + loss_ds)
+            loss_sum += loss.item() * batch_size_s
+
+            model.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # logger.info('epoch: {:>4}, [iter: {:>4} / all {:>4}], loss {:8.4f}, '
+            #             'loss_c: {:8.4f}, loss_ds: {:8.4f}, loss_dt: {:8.4f}\n'
+            #             .format(epoch, i+1, len_dataloader, loss.item(), loss_c.item(), loss_ds.item(), loss_dt.item()))
+
+        tr_acc, tr_f1 = evaluate_dann(model, dataset_source, batch_size)
+        te_acc, te_f1 = evaluate_dann(model, dataset_target, batch_size)
+        tr_acc_ls.append(tr_acc)
+        te_acc_ls.append(te_acc)
+        loss_ls.append(loss_sum)
+        # If find a better result, save the model
+        if te_acc > best_acc:
+            best_acc = te_acc
+            best_ep = epoch
+            checkpoint = {"epoch": epoch, "state_dict": model.state_dict()}
+            torch.save(checkpoint, ckpt_save_path + '.ckpt')
+
+        logger.info('epoch: {:>4}, loss: {:8.4f}, train acc: {:8.4f}, train f1: {:8.4f},'
+                    ' eval acc: {:8.4f}, eval f1: {:8.4f}'
+                    .format(epoch, loss_sum, tr_acc, tr_f1, te_acc, te_f1))
+
+    logger.info('='*10)
+    logger.info('best epoch: {:>4}, best acc: {:8.4f}'.format(best_ep, best_acc))
+    pickle.dump(tr_acc_ls, open(ckpt_save_path + '.tracc', 'wb'))
+    pickle.dump(te_acc_ls, open(ckpt_save_path + '.teacc', 'wb'))
+    pickle.dump(loss_ls, open(ckpt_save_path + '.loss', 'wb'))
+
+
+def main():
+    with open('./dataset/seed/data.pkl', 'rb') as f:
+        data = pickle.load(f)
+        subject_ls = list(data.keys())
+        # Iterate over all folds
+        for sub in subject_ls:
+            # Construct dataset
+            target_data = data[sub]
+            dataset_target = SeedDataset(target_data['data'], target_data['label'])
+            dataset_source = SeedDataset(
+                data=np.concatenate([data[sub]['data'] for s in subject_ls if s != sub], axis=0),
+                label=np.concatenate([data[sub]['label'] for s in subject_ls if s != sub], axis=0)
+            )
+            logger.info(sub + ' dataset_source: {}, dataset_target: {} built!'
+                        .format(len(dataset_source), len(dataset_target)))
+            # Train this fold
+            train_dann(dataset_source, dataset_target, 1000, 32, 310, [128, 128], 3, './models/{}'.format(sub))
+
+
+if __name__ == '__main__':
+    manual_seed = 0
+    random.seed(manual_seed)
+    torch.manual_seed(manual_seed)
+    main()
+
--- DANN/model.py
+++ DANN/model.py
@@ -3,44 +3,45 @@ from functions import ReverseLayerF
 
 
 class CNNModel(nn.Module):
-
-    def __init__(self):
+    def __init__(self, in_dim, h_dims, out_dim):
         super(CNNModel, self).__init__()
+        self.h_dims = h_dims
+        self.in_dim = in_dim
+        self.out_dim = out_dim
         self.feature = nn.Sequential()
-        self.feature.add_module('f_conv1', nn.Conv2d(3, 64, kernel_size=5))
-        self.feature.add_module('f_bn1', nn.BatchNorm2d(64))
-        self.feature.add_module('f_pool1', nn.MaxPool2d(2))
-        self.feature.add_module('f_relu1', nn.ReLU(True))
-        self.feature.add_module('f_conv2', nn.Conv2d(64, 50, kernel_size=5))
-        self.feature.add_module('f_bn2', nn.BatchNorm2d(50))
-        self.feature.add_module('f_drop1', nn.Dropout2d())
-        self.feature.add_module('f_pool2', nn.MaxPool2d(2))
-        self.feature.add_module('f_relu2', nn.ReLU(True))
+
+        self.feature.add_module('f_fc0', nn.Linear(in_dim, h_dims[0]))
+        self.feature.add_module('f_bn0', nn.BatchNorm1d(h_dims[0]))
+        self.feature.add_module('f_relu0', nn.LeakyReLU())
+        for i in range(1, len(h_dims)):
+            self.feature.add_module('f_fc{}'.format(i), nn.Linear(h_dims[i-1], h_dims[i]))
+            self.feature.add_module('f_bn{}'.format(i), nn.BatchNorm1d(h_dims[i]))
+            self.feature.add_module('f_relu{}'.format(i), nn.LeakyReLU())
 
         self.class_classifier = nn.Sequential()
-        self.class_classifier.add_module('c_fc1', nn.Linear(50 * 4 * 4, 100))
-        self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(100))
-        self.class_classifier.add_module('c_relu1', nn.ReLU(True))
-        self.class_classifier.add_module('c_drop1', nn.Dropout())
-        self.class_classifier.add_module('c_fc2', nn.Linear(100, 100))
-        self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(100))
-        self.class_classifier.add_module('c_relu2', nn.ReLU(True))
-        self.class_classifier.add_module('c_fc3', nn.Linear(100, 10))
-        self.class_classifier.add_module('c_softmax', nn.LogSoftmax(dim=1))
+        self.class_classifier.add_module('c_fc1', nn.Linear(h_dims[-1], 64))
+        self.class_classifier.add_module('c_bn1', nn.BatchNorm1d(64))
+        self.class_classifier.add_module('c_relu1', nn.LeakyReLU(True))
+        # self.class_classifier.add_module('c_drop1', nn.Dropout())
+        self.class_classifier.add_module('c_fc2', nn.Linear(64, 64))
+        self.class_classifier.add_module('c_bn2', nn.BatchNorm1d(64))
+        self.class_classifier.add_module('c_relu2', nn.LeakyReLU(True))
+        self.class_classifier.add_module('c_fc3', nn.Linear(64, out_dim))
+        # self.class_classifier.add_module('c_softmax', nn.LogSoftmax(dim=1))
 
         self.domain_classifier = nn.Sequential()
-        self.domain_classifier.add_module('d_fc1', nn.Linear(50 * 4 * 4, 100))
-        self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(100))
-        self.domain_classifier.add_module('d_relu1', nn.ReLU(True))
-        self.domain_classifier.add_module('d_fc2', nn.Linear(100, 2))
-        self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
+        self.domain_classifier.add_module('d_fc1', nn.Linear(h_dims[-1], 64))
+        self.domain_classifier.add_module('d_bn1', nn.BatchNorm1d(64))
+        self.domain_classifier.add_module('d_relu1', nn.LeakyReLU(True))
+        self.domain_classifier.add_module('d_fc2', nn.Linear(64, 64))
+        self.domain_classifier.add_module('d_bn2', nn.BatchNorm1d(64))
+        self.domain_classifier.add_module('d_relu2', nn.LeakyReLU(True))
+        self.domain_classifier.add_module('d_fc3', nn.Linear(64, 2))
+        # self.domain_classifier.add_module('d_softmax', nn.LogSoftmax(dim=1))
 
     def forward(self, input_data, alpha):
-        input_data = input_data.expand(input_data.data.shape[0], 3, 28, 28)
         feature = self.feature(input_data)
-        feature = feature.view(-1, 50 * 4 * 4)
         reverse_feature = ReverseLayerF.apply(feature, alpha)
         class_output = self.class_classifier(feature)
         domain_output = self.domain_classifier(reverse_feature)
-
         return class_output, domain_output
